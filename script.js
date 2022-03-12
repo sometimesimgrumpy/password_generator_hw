4971f0fb9033d7f9a91d570454cc16e3df01a0b5
Brianna <briannaodell1437@gmail.com>
2022-03-11 21:43:23 -0800

    Final java coding

--- script.js
+++ script.js
@@ -17,150 +17,127 @@ generateBtn.addEventListener("click", writePassword);
 //create buckets for each character type - upper, lower, special characters, numbers
 //reference https://gist.github.com/bendc/1e6af8f2d8027f2965da
 
-var lowerCase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-];
-var upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A",  "B",  "C", "D", "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L", "M",  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "+",
-  "-",
-  ".",
-  "~",
-  "|",
-  "<",
-  ">",
-  "=",
-  "-",
-  "_",
-  "/",
-  ":",
-  ";",
-  "?",
-  "[",
-  "]",
-  "{",
-  "}",
-  "~"
-];
+var specialChars = ["!","@", "#",  "$",  "%",  "^",  "&",  "*",  "(",  ")",  "+",  "-",  ".",  "~",  "|",  "<",  ">",  "=",  "-",  "_",  "/",  ":",  ";",  "?",  "[",  "]",  "{",  "}",  "~"];
+var userChoice;
 
 //create function for generatePassword
-
-//identify and collect buckets user has chosen
 
 function generatePassword() {
   //get user preferences from password
   //ask user about how many characters
-  var characterLimit = window.prompt(
-    "How many characters would you like the password to be?"
-  );
+  var characterLimit = window.prompt("How many characters would you like the password to be?");
 
   //if user doesn't include any number - reference https://stackoverflow.com/questions/20533127/detect-empty-value-on-prompt
   if (characterLimit === "") {
     window.alert("Please enter the character length for your new password.");
-  } else if (characterLimit != null) {
-    //user enters number
-    var askCapLetters = window.confirm(
-      "Would you like to include uppercase letters?"
-    );
+  } 
+  else if (characterLimit != null) {                         //user enters number
+    var askCapLetters = window.confirm("Would you like to include uppercase letters?");
   }
 
   if (askCapLetters === true) {
-    //ask for capital letters
-    var askUpLetters = window.confirm(
-      "Would you like to include lowercase letters?"
-    );
+      //ask for lower case letters
+    var askLowLetters = window.confirm("Would you like to include lowercase letters?");
+  }
 
-  if (askUpLetters === true) {
-      //ask for uppercase letters
+  if (askLowLetters === true) {
+      //ask for numbers
     var askNumbers = window.confirm("Would you like to include numbers?");
   }
 
   if (askNumbers === true) {
-      //ask for numbers
-    var askSpecial = window.confirm(
-      "Would you like to include special characters?"
-  );
-      
-    //create guaranteed collection, array
-      var passArray = []
-      
-    //create a password variable
-      var password = "";
-      
-    //randomly draw characters to create a password
-      
-    //put that character on the password
-    
-    //repeat as many times as requested by user, for loops
-      
-    //loop through the guaranteed and replace elements in the password array
-      
-    //join characters in the password array into a string
-      
-    //return the password string 
-    }
+      //ask for special characters
+    var askSpecial = window.confirm("Would you like to include special characters?");
   }
+
+  if (askSpecial === true) {
+    window.alert("Thank you for making a password!")
+  }
+
+  //no options selected
+  if (!askCapLetters && !askLowLetters && !askNumbers && !askSpecial) {
+    userChoice = window.alert("No choices selected, please select at least one option.")
+  }
+
+  //All options selected
+  else if (askCapLetters && askLowLetters && askNumbers && askSpecial) {
+    userChoice = upperCase.concat(upperCase, lowerCase, numbers, specialChars)
+  }
+
+
+  //3 options
+  else if (askCapLetters && askLowLetters && askNumbers) {
+    userChoice = upperCase.concat(upperCase, lowerCase, numbers)
+  }
+  else if (askCapLetters && askLowLetters && askSpecial) {
+    userChoice = upperCase.concat(upperCase, lowerCase, specialChars)
+  }
+  else if (askCapLetters && askNumbers && askSpecial) {
+    userChoice = upperCase.concat(upperCase, numbers, specialChars)
+  }
+  else if (askLowLetters && askNumbers && askSpecial) {
+    userChoice = lowerCase.concat(lowerCase, numbers, specialChars)
+  }
+
+
+  //2 options
+  else if (askCapLetters && askLowLetters) {
+    userChoice = upperCase.concat(upperCase, lowerCase)
+  }
+  else if (askCapLetters && askNumbers) {
+    userChoice = upperCase.concat(upperCase, numbers)
+  }
+  else if (askLowLetters && askNumbers) {
+    userChoice = lowerCase.concat(lowerCase, numbers)
+  }
+  else if (askLowLetters && askSpecial) {
+    userChoice = lowerCase.concat(lowerCase, specialChars)
+  }
+  else if (askNumbers && askSpecial) {
+    userChoice = numbers.concat(numbers, specialChars)
+  }
+  else if (askCapLetters && askSpecial) {
+    userChoice = upperCase.concat(upperCase, specialChars)
+  }
+
+  //1 option
+  else if (askCapLetters) {
+    userChoice = upperCase
+  }
+  else if (askSpecial) {
+    userChoice = specialChars
+  }
+  else if (askLowLetters) {
+    userChoice = lowerCase
+  }
+  else if (askNumbers) {
+    userChoice = numbers
+  }
+
+//create guaranteed collection, array
+var passArray = [];
+
+//randomly draw characters to create a password, repeat by user preference, loop through as many times as needed
+for (var i=0; i < characterLimit; i++) {
+  var userData = userChoice[Math.floor(Math.random() * userChoice.length)];
+  passArray.push(userData);
 }
+
+//create a password variable and join
+var finalPassword = passArray.join("")
+
+//to see what was chosen
+console.log(passArray)
+
+//return the password string
+return finalPassword;
+
+
+//Thank you to Travis for helping me :)
+  }
+ 
+
+
